1c482107d8fdced9dc4265679fd5491cf403cf6d
yaozhixing <yaozhixing797600@163.com>
2020-05-17 16:40:35 +0800

    【admin】初始化架构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import Utils from './common/utils'
+import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import './mock.js'
+import './assets/css/main.scss'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$http = axios
+Vue.prototype.Utils = Utils
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
