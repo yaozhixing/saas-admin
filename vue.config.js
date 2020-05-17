dd3f3563384661b33a20b92345f3c460463f3141
yaozhixing <yaozhixing797600@163.com>
2020-05-18 00:13:29 +0800

    修改dist名

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  //输出文件目录
+  outputDir: 'saas-admin',
 }
